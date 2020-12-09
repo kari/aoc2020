0454a00b72587beaa0d46a204771398a35bbd364
Kari Silvennoinen <kari@kalifi.org>
2020-12-09 09:51:25 +0200

    day 9

--- day09.jl
+++ day09.jl
@@ -4,20 +4,40 @@ numbers = open("xmas_cypher.txt") do f
     parse.(Int, readlines(f))
 end
 
-preamble = 25
-for i in (preamble+1):length(numbers)
-    found = false
-    n = numbers[i]
-    prev = numbers[(i-preamble):(i-1)]
-    for pair in combinations(prev, 2)
-        if sum(pair) == n
-            # println(pair, n)
-            found = true
-            break
+function find_weak(numbers::Array{Int64, 1}, preamble::Int64)::Int64
+    for i in (preamble+1):length(numbers)
+        found = false
+        n = numbers[i]
+ 
+        for pair in combinations(numbers[(i-preamble):(i-1)], 2)
+            if sum(pair) == n
+                found = true
+                break
+            end
+        end
+ 
+        if !found
+            return n
         end
     end
-    if !found
-        println(n)
-        break
-    end
+
+    return 0
 end
+
+function find_contiguous_set(numbers::Array{Int64,1}, sum_to::Int64)::Array{Int64, 1}
+   
+    for i in 1:(length(numbers)-1)
+        for j in (i+1):length(numbers)
+            if sum(numbers[i:j]) == sum_to
+                return numbers[i:j]
+            end
+        end
+    end
+
+    return []
+end
+
+weak = find_weak(numbers, 25)
+println(weak)
+set = find_contiguous_set(numbers, weak)
+println(minimum(set) + maximum(set))
