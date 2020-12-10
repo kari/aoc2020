969adfebfbbb1f3c6ab05f4ab1f3811b756b0d7f
Kari Silvennoinen <kari@kalifi.org>
2020-12-10 13:53:53 +0200

    day 10 refactor

--- day10.jl
+++ day10.jl
@@ -2,19 +2,19 @@ adapters = open("adapters.txt") do f
     sort(parse.(Int, readlines(f)))
 end
 
-function find_contiguous_sets(numbers::Array{Int64,1})::Array{Int64, 1}
+function find_contiguous_sets(numbers::Array{Int64,1}, n::Int64)::Array{Int64, 1}
     sets = []
     i = 1
 
     while i < length(numbers)
-        if numbers[i] != 1
+        if numbers[i] != n
             i += 1
             continue
         end
 
         j = i
         while j < length(numbers)
-            if numbers[j+1] != 1
+            if numbers[j+1] != n
                 break
             end
             j += 1
@@ -31,18 +31,23 @@ function find_contiguous_sets(numbers::Array{Int64,1})::Array{Int64, 1}
 end
 
 function permutations(x::Int64)::Int64
-    if x == 2
-        return 2 # [1,1], [2]
-    elseif x == 3
-        return 4 # [1,1,1], [2,1], [1,2], [3]
-    elseif x == 4
-        return 7 # [1,1,1,1], [2,1,1], [1,2,1], [1,1,2], [2,2] [1,3], [3,1]
-    else
-        throw(DomainError(x, "Unhandled length"))
+    sum = 0
+
+    for i in (x ÷ 3):-1:0
+        y = x - 3*i
+
+        for j in (y ÷ 2):-1:0
+            z = y - 2*j
+            p = factorial(z+j+i) ÷ (factorial(z)*factorial(j)*factorial(i))
+            sum += p
+            # println("$(x): [$(z), $(j), $(i)]: $(p)")
+        end
     end
+
+    return sum
 end
 
 pushfirst!(adapters, 0)
 diffs = push!(map(i -> adapters[i]-adapters[i-1], 2:length(adapters)), 3)
 println(count(i -> i == 1, diffs) * count(i -> i == 3, diffs))
-println(prod(map(x -> permutations(x), find_contiguous_sets(diffs))))
+println(prod(map(x -> permutations(x), find_contiguous_sets(diffs, 1))))
