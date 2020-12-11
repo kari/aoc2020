91981f2de7c7673ea8be721e3aa1c3dfffaeeb1c
Kari Silvennoinen <kari@kalifi.org>
2020-12-11 16:16:37 +0200

    day 11

--- day11.jl
+++ day11.jl
@@ -47,7 +47,29 @@ function count_neighborhood(arr, x::Int64, y::Int64, c::Char)::Int64
     return n
 end
 
-function simulate_neighborhood(arr)
+function count_visible(arr, x::Int64, y::Int64, c::Char)::Int64
+    n = 0
+    unit_vectors = map(x -> [round(Int,sin(x)),round(Int,cos(x))], pi*(-1:1/4:0.9))
+    for uv in unit_vectors
+        l = 1
+        while true
+            ox, oy = l * uv
+            if y+oy < 1 || y+oy > length(arr) || x+ox < 1 || x+ox > length(arr[1])
+                break
+            end
+            if arr[y+oy][x+ox] == c
+                n += 1
+            end
+            if arr[y+oy][x+ox] != '.'
+                break
+            end
+            l += 1
+        end
+    end
+    return n
+end
+
+function simulate(arr, fn, n)
     step = deepcopy(arr)
 
     for y in 1:length(arr)
@@ -56,13 +78,13 @@ function simulate_neighborhood(arr)
             if arr[y][x] == '.'
                 # println("do nothing")
             elseif arr[y][x] == 'L'
-                occupied = count_neighborhood(arr, x, y, '#')
+                occupied = fn(arr, x, y, '#')
                 if occupied == 0
                     step[y][x] = '#'
                 end
             elseif arr[y][x] == '#'
-                occupied = count_neighborhood(arr, x, y, '#')
-                if occupied >= 4
+                occupied = fn(arr, x, y, '#')
+                if occupied >= n
                     step[y][x] = 'L'
                 end
             end
@@ -72,11 +94,11 @@ function simulate_neighborhood(arr)
     return step
 end
 
-function run_neighborhood(state)
+function run(state, fn, n)
     i = 0
     while true
         i += 1
-        next_state = simulate_neighborhood(state)
+        next_state = simulate(state, fn, n)
         if state == next_state
             break
         end
@@ -86,4 +108,5 @@ function run_neighborhood(state)
     return state    
 end
 
-println(sum(map(r -> count(x -> x == '#', r), run_neighborhood(seating))))
+println(sum(map(r -> count(x -> x == '#', r), run(seating, count_neighborhood, 4))))
+println(sum(map(r -> count(x -> x == '#', r), run(seating, count_visible, 5))))
