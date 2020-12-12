a63a97b98591c6a84f6c605b42978e561d15a80e
Kari Silvennoinen <kari@kalifi.org>
2020-12-12 09:45:32 +0200

    day 12

--- day12.jl
+++ day12.jl
@@ -1,30 +1,63 @@
-mutable struct Ship
-    direction::Int64
-    position::Vector{Int64}
-end
-
 instructions = open("navigation_data.txt") do f
     readlines(f)
 end
 
-ship = Ship(0, [0, 0])
+function rotate(v::Vector{Int64}, a::Int64)::Vector{Int64}
+    b = deg2rad(a)
+    x1, y1 = v
+    x2 = round(Int, cos(b)*x1 - sin(b)*y1)
+    y2 = round(Int, sin(b)*x1 + cos(b)*y1)
+
+    return [x2, y2]
+end
+
+ship = [0, 0]
+direction = 0
 for i in instructions
+    global ship, direction
+
     cmd, val = i[1], parse(Int, i[2:end])
     if cmd == 'N'
-        ship.position = ship.position + [0, val]
+        ship += [0, val]
     elseif cmd == 'E'
-        ship.position = ship.position + [val, 0]
+        ship += [val, 0]
     elseif cmd == 'S'
-        ship.position = ship.position + [0, -val]
+        ship += [0, -val]
     elseif cmd == 'W'
-        ship.position = ship.position + [-val, 0]
+        ship += [-val, 0]
     elseif cmd == 'F'
-        ship.position = ship.position + [val*round(Int, cos(deg2rad(ship.direction))), val*round(Int, sin(deg2rad(ship.direction)))]
+        ship += [val*round(Int, cos(deg2rad(direction))), val*round(Int, sin(deg2rad(direction)))]
     elseif cmd == 'L'
-        ship.direction = (ship.direction + val) % 360
+        direction = (direction + val) % 360
     elseif cmd == 'R'
-        ship.direction = (ship.direction - val) % 360
+        direction = (direction - val) % 360
     end
 end
 
-println(sum(abs.(ship.position)))
+println(sum(abs.(ship)))
+
+waypoint = [10, 1] # relative to ship
+ship = [0,0]
+for i in instructions
+    global ship, waypoint
+
+    cmd, val = i[1], parse(Int, i[2:end])
+    if cmd == 'N'
+        waypoint += [0, val]
+    elseif cmd == 'E'
+        waypoint += [val, 0]
+    elseif cmd == 'S'
+        waypoint += [0, -val]
+    elseif cmd == 'W'
+        waypoint += [-val, 0]
+    elseif cmd == 'F'
+        ship += waypoint * val
+    elseif cmd == 'L'
+        waypoint = rotate(waypoint, val)
+    elseif cmd == 'R'
+        waypoint = rotate(waypoint, -val)
+    end
+    # println("$(i): ship: $(ship), wp: $(waypoint)")
+end
+
+println(sum(abs.(ship)))
