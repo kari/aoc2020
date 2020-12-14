2fb662401fc164c09a6646a05454be05c0d603f0
Kari Silvennoinen <kari@kalifi.org>
2020-12-14 21:38:59 +0200

    day 14 refactor

--- day14.jl
+++ day14.jl
@@ -3,58 +3,42 @@ lines = open("bitmasks.txt") do f
 end
 
 memory = zeros(Int64, 100000)
-zeromask = ""
-onemask = ""
+zeromask = nothing
+onemask = nothing
 for l in lines
     global memory, zeromask, onemask
-    if startswith(l, "mask = ")    
+    if startswith(l, "mask = ")
         mask = match(r"[10X]+", l).match
-        onemask = parse(Int, replace(mask, "X" => "0"); base = 2) # OR
-        zeromask = parse(Int, replace(mask, "X" => "1"); base = 2) # AND
+        onemask = parse(Int, replace(mask, "X" => "0"); base = 2)
+        zeromask = parse(Int, replace(mask, "X" => "1"); base = 2)
     else
         addr, val = parse.(Int, match(r"mem\[(\d+)\] = (\d+)", l).captures)
-        res = val & zeromask | onemask
-        # println("value:  $(bitstring(val)[end-35:end]) (decimal $(val))")
-        # println("mask:   $(mask)")
-        # println("result: $(bitstring(res)[end-35:end])")
-        memory[addr] = res
+        memory[addr] = val & zeromask | onemask
     end
 end
 
 println(sum(memory))
 
 memory = Dict{Int64, Int64}()
-onemask = ""
-xmask = []
+onemask = nothing
+xmask = nothing
 for l in lines
     global memory, xmask, onemask
-    if startswith(l, "mask = ")    
+    if startswith(l, "mask = ")
         mask = match(r"[10X]+", l).match
-        onemask = parse(Int, replace(mask, "X" => "0"); base = 2) # OR
+        onemask = parse(Int, replace(mask, "X" => "0"); base = 2)
         xmask = findall(x -> x == 'X', mask)
     else
         addr, val = parse.(Int, match(r"mem\[(\d+)\] = (\d+)", l).captures)
-        res = addr | onemask
-        res = collect(bitstring(res)[end-35:end])
-        # for i in xmask
-        #     res[i] = 'X'
-        # end
-        # res = join(res)
-        # println("address: $(bitstring(addr)[end-35:end]) (decimal $(addr))")
-        # println("mask:    $(mask)")
-        # println("result:  $(res)")
+        res = collect(bitstring(addr | onemask)[end-35:end])
         for i in 0:(2^length(xmask)-1)
             floating = bitstring(i)[end-(length(xmask)-1):end]
             float = collect(res)
             for i in 1:length(xmask)
                 float[xmask[i]] = floating[i]
             end
-            float = join(float)
-            n = parse(Int, float; base = 2)
-            # println("float:   $(float) (decimal $(n))")
-            memory[n] = val
+            memory[parse(Int, join(float); base = 2)] = val
         end
-        # println()
     end
 end
 
