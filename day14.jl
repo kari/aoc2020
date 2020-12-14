6bfbf19b8cf3a07c1b8c7ecb18058890bce02326
Kari Silvennoinen <kari@kalifi.org>
2020-12-14 16:21:16 +0200

    day 14 refactor

--- day14.jl
+++ day14.jl
@@ -5,9 +5,8 @@ end
 memory = zeros(Int64, 100000)
 zeromask = ""
 onemask = ""
-mask = ""
 for l in lines
-    global memory, zeromask, onemask, mask
+    global memory, zeromask, onemask
     if startswith(l, "mask = ")    
         mask = match(r"[10X]+", l).match
         onemask = parse(Int, replace(mask, "X" => "0"); base = 2) # OR
@@ -27,9 +26,8 @@ println(sum(memory))
 memory = Dict{Int64, Int64}()
 onemask = ""
 xmask = []
-mask = ""
 for l in lines
-    global memory, xmask, onemask, mask
+    global memory, xmask, onemask
     if startswith(l, "mask = ")    
         mask = match(r"[10X]+", l).match
         onemask = parse(Int, replace(mask, "X" => "0"); base = 2) # OR
@@ -38,15 +36,15 @@ for l in lines
         addr, val = parse.(Int, match(r"mem\[(\d+)\] = (\d+)", l).captures)
         res = addr | onemask
         res = collect(bitstring(res)[end-35:end])
-        for i in xmask
-            res[i] = 'X'
-        end
-        res = join(res)
+        # for i in xmask
+        #     res[i] = 'X'
+        # end
+        # res = join(res)
         # println("address: $(bitstring(addr)[end-35:end]) (decimal $(addr))")
         # println("mask:    $(mask)")
         # println("result:  $(res)")
         for i in 0:(2^length(xmask)-1)
-            floating = collect(bitstring(i)[end-(length(xmask)-1):end])
+            floating = bitstring(i)[end-(length(xmask)-1):end]
             float = collect(res)
             for i in 1:length(xmask)
                 float[xmask[i]] = floating[i]
