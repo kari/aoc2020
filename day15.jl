c4a9f89f49817ab7298f6de57c6afea19b671426
Kari Silvennoinen <kari@kalifi.org>
2020-12-15 10:18:00 +0200

    day 15

--- day15.jl
+++ day15.jl
@@ -2,19 +2,50 @@ starting_numbers = open("starting_numbers.txt") do f
     parse.(Int, split(readline(f), ","))
 end
 
-# starting_numbers = [0,3,6]
-
-for i in (length(starting_numbers)+1):2020
-    global D, starting_numbers
-    prev = starting_numbers[end]
-    if count(x -> x == prev, starting_numbers) == 1
-        x = 0
-    else
-        recent = findprev(x -> x == prev, starting_numbers, length(starting_numbers)-1)
-        x = length(starting_numbers)-recent
+function play_game(starting::Vector{Int64}, upto::Int64)::Int64
+    counts = Dict{Int64, Int64}()
+    last_seen = Dict{Int64, Tuple{Int64, Int64}}()
+    prev = starting[end]
+    
+    for (i, v) = enumerate(starting)
+        counts[v] = 1
+        last_seen[v] = (i, i)
+        # println("Turn $(i): $(v)")
     end
-    # println("Turn $(i): $(x)")
-    push!(starting_numbers, x)
+
+    for i in (length(starting)+1):upto
+        # if i % 1000000 == 0
+        #     println(i)
+        # end
+        if counts[prev] == 1
+            x = 0
+        else
+            recent = last_seen[prev][2]
+            x = i-1-recent
+            # println("$(prev): $(i-1) - $(recent)")
+        end
+        # println("Turn $(i): $(x)")
+        if haskey(counts, x)
+            counts[x] += 1
+            last_seen[x] = (i, last_seen[x][1])
+        else
+            counts[x] = 1
+            last_seen[x] = (i, i)
+        end
+        prev = x
+    end
+
+    return prev
 end
 
-println(spoken[2020])
+# println(play_game([0,3,6], 10) == 0)
+# println(play_game([0,3,6], 2020) == 436)
+# println(play_game([1,3,2], 2020) == 1)
+# println(play_game([2,1,3], 2020) == 10)
+# println(play_game([1,2,3], 2020) == 27)
+# println(play_game([2,3,1], 2020) == 78)
+# println(play_game([3,2,1], 2020) == 438)
+# println(play_game([3,1,2], 2020) == 1836)
+
+println(play_game(starting_numbers, 2020))
+println(play_game(starting_numbers, 30000000))
