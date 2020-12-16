7360b940432027ba2529947785501ffff9989c47
Kari Silvennoinen <kari@kalifi.org>
2020-12-16 20:29:42 +0200

    day 06 refactor

--- day06.jl
+++ day06.jl
@@ -2,6 +2,6 @@ answers = open("customs_answers.txt") do f
     map(row -> split(row, "\n"), split(read(f, String), "\n\n"))
 end
 
-println(sum(map(answer -> length(reduce(union, Set.(answer))), answers)))
+println(sum(map(answer -> length(union(Set.(answer)...)), answers)))
 
-println(sum(map(answer -> length(reduce(intersect, Set.(answer))), answers)))
+println(sum(map(answer -> length(intersect(Set.(answer)...)), answers)))
