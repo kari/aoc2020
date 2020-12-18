9eed67d195898c40cf9a78d548655edebdc8fa82
Kari Silvennoinen <kari@kalifi.org>
2020-12-18 15:40:20 +0200

    day 18

--- day18.jl
+++ day18.jl
@@ -30,4 +30,29 @@ function eval(eq::AbstractString)
     return acc
 end
 
+function eval2(eq::AbstractString)
+    # println(eq)
+    paren = r"\((?:[^)(]+|(?R))*+\)"
+    for m in reverse(collect(eachmatch(paren, eq)))
+        neq = m.match[2:end-1]
+        eq = replace(eq, m.match => eval2(neq))
+    end
+    while occursin("+", eq)
+        m = last(collect(eachmatch(r"(\d+) \+ (\d+)", eq)))
+        o1, o2 = parse.(Int, m.captures)
+        eq = replace(eq, m.match => o1 + o2)
+        # println("$(o1) + $(o2) => $(o1 + o2)")
+    end
+    # println(eq)
+    while occursin("*", eq)
+        m = last(collect(eachmatch(r"(\d+) \* (\d+)", eq)))
+        o1, o2 = parse.(Int, m.captures)
+        eq = replace(eq, m.match => o1 * o2)
+        # println("$(o1) * $(o2) => $(o1 * o2)")
+    end
+    return parse(Int, eq)
+end
+
 println(sum(map(eq -> eval(eq), equations)))
+
+println(sum(map(eq -> eval2(eq), equations)))
