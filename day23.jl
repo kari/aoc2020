160828750f43cf668ebbe67001feb2af221aae79
Kari Silvennoinen <kari@kalifi.org>
2020-12-24 15:42:12 +0200

    naive day 23 part 2

--- day23.jl
+++ day23.jl
@@ -1,41 +1,55 @@
 input = "318946572"
-#input = "389125467"
-cups = parse.(Int, collect(input))
-min_cup, max_cup = extrema(cups)
+input = "389125467" # test pattern
+orig_cups = parse.(Int, collect(input))
 
-cup_idx = 1
-for i in 1:100
-    global cup_idx, cups, min_cup, max_cup
-    # println("-- move $(i) --")
-    # println("cups: $(join(cups, " "))")
-    cup = cups[cup_idx]
-    # println("current cup: $(cup)")
-    picked = []
-    for j in 1:3
-        p = cups[(j+cup_idx-1) % length(cups) + 1]
-        push!(picked, p)
-    end
-    # remove picks from cups
-    filter!(c -> c ∉ picked, cups)
-    # println("pick up: $(join(picked, " "))")
-    destination = cup - 1
-    if destination < min_cup
-        destination = max_cup
-    end
-    while destination in picked
-        destination -= 1
+function play(cups, n)
+    cup_idx = 1
+    min_cup, max_cup = extrema(cups)
+
+    for i in 1:n
+        if (i % 100_000 == 0)
+            println(i)
+        end
+        # println("-- move $(i) --")
+        # println("cups: $(join(cups, " "))")
+        cup = cups[cup_idx]
+        # println("current cup: $(cup)")
+        picked = []
+        for j in 1:3
+            p = cups[(j+cup_idx-1) % length(cups) + 1]
+            push!(picked, p)
+        end
+        # remove picks from cups
+        filter!(c -> c ∉ picked, cups)
+        # println("pick up: $(join(picked, " "))")
+        destination = cup - 1
         if destination < min_cup
             destination = max_cup
         end
+        while destination in picked
+            destination -= 1
+            if destination < min_cup
+                destination = max_cup
+            end
+        end
+        # println("destination cup: $(destination)")
+        idx = findfirst(x -> x == destination, cups)
+        # insert cups
+        for j in 1:3
+            insert!(cups, idx+j, picked[j])
+        end
+        cup_idx = findfirst(x -> x == cup, cups) % length(cups) + 1
     end
-    # println("destination cup: $(destination)")
-    idx = findfirst(x -> x == destination, cups)
-    # insert cups
-    for j in 1:3
-        insert!(cups, idx+j, picked[j])
-    end
-    cup_idx = findfirst(x -> x == cup, cups) % length(cups) + 1
+
+    return first_idx = findfirst(x -> x == 1, cups)
+
 end
 
-first_idx = findfirst(x -> x == 1, cups)
-println(join(vcat(cups[(first_idx+1):end], cups[1:(first_idx-1)])))
+cups = copy(orig_cups)
+i = play(cups, 100)
+println(join(vcat(cups[(i+1):end], cups[1:(i-1)])))
+
+cups = vcat(copy(orig_cups), collect((maximum(orig_cups)+1):1000000))
+i = play(cups, 10000000)
+println(cups[i+1])
+println(cups[i+2])
