af6de8cbdc685a343e79fb2843ac7d436e6d5071
Kari Silvennoinen <kari@kalifi.org>
2020-12-27 12:12:32 +0200

    day 23

--- day23.jl
+++ day23.jl
@@ -1,10 +1,13 @@
+using Profile
+
 input = "318946572"
-input = "389125467" # test pattern
+# input = "389125467" # test pattern
 orig_cups = parse.(Int, collect(input))
 
 function play(cups, n)
     cup_idx = 1
     min_cup, max_cup = extrema(cups)
+    picked = Array{Int64, 1}(undef, 3)
 
     for i in 1:n
         if (i % 100_000 == 0)
@@ -12,16 +15,13 @@ function play(cups, n)
         end
         # println("-- move $(i) --")
         # println("cups: $(join(cups, " "))")
-        cup = cups[cup_idx]
+        cup = cups[1]
         # println("current cup: $(cup)")
-        picked = []
-        for j in 1:3
-            p = cups[(j+cup_idx-1) % length(cups) + 1]
-            push!(picked, p)
-        end
+        pick_idxs = 2:4
+        picked = cups[pick_idxs]
         # remove picks from cups
-        filter!(c -> c ∉ picked, cups)
-        # println("pick up: $(join(picked, " "))")
+#        filter!(c -> c ∉ picked, cups)
+        # println("pick up: $(join(picked, " ")) ($(join(pick_idxs, " ")))")
         destination = cup - 1
         if destination < min_cup
             destination = max_cup
@@ -32,24 +32,64 @@ function play(cups, n)
                 destination = max_cup
             end
         end
-        # println("destination cup: $(destination)")
-        idx = findfirst(x -> x == destination, cups)
+        dst_idx = findfirst(x -> x == destination, cups)
+        # println("destination cup: $(destination) ($(dst_idx))")
         # insert cups
-        for j in 1:3
-            insert!(cups, idx+j, picked[j])
-        end
-        cup_idx = findfirst(x -> x == cup, cups) % length(cups) + 1
+        cups[2:dst_idx-3] = cups[5:dst_idx]
+        # println(cups)
+        cups[(dst_idx-2):(dst_idx)] = picked
+        # println(cups)
+        cups[1:end-1] = cups[2:end]
+        cups[end] = cup
     end
 
     return first_idx = findfirst(x -> x == 1, cups)
+end
 
+function play2(cups, n)
+    list = Array{Int64, 1}(undef, length(cups))
+    for i in 1:(length(cups)-1)
+        list[cups[i]] = cups[i+1]
+    end
+    list[cups[end]] = cups[1]
+    # println(list)
+    cup = cups[1]
+    min_cup, max_cup = extrema(cups)
+    for i in 1:n
+        # println("-- move $(i) --")
+        # println("cup: ", cup)
+        picked = [list[cup], list[list[cup]], list[list[list[cup]]]]
+        # println("pick up: ", join(picked, ", "))
+        destination = cup - 1
+        if destination < min_cup
+            destination = max_cup
+        end
+        while destination in picked
+            destination -= 1
+            if destination < min_cup
+                destination = max_cup
+            end
+        end
+        # println("destination: ", destination)
+        list[cup] = list[picked[end]]
+        list[picked[end]] = list[destination]
+        list[destination] = picked[1]
+        cup = list[cup]
+    end
+
+    idx = 1
+    for i in 1:(length(cups))
+        cups[i] = list[idx]
+        idx = list[idx]
+    end
+
+    return cups
 end
 
 cups = copy(orig_cups)
 i = play(cups, 100)
 println(join(vcat(cups[(i+1):end], cups[1:(i-1)])))
 
-cups = vcat(copy(orig_cups), collect((maximum(orig_cups)+1):1000000))
-i = play(cups, 10000000)
-println(cups[i+1])
-println(cups[i+2])
+cups = vcat(copy(orig_cups), collect((maximum(orig_cups)+1):1_000_000))
+play2(cups, 10_000_000)
+println(cups[1]*cups[2])
